db27db8c000843b013424ef99ee050b222338d8d
Mansi Chouhan <mansichouhan6789@gmail.com>
2024-11-02 19:53:56 +0530

    Add files via upload

---
+++ ConcateString.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class ConcateString {
+    public static void main(String args[]){
+     String s1,s2;
+     Scanner sc=new Scanner(System.in);
+     System.out.println("Enter 1st string");
+     s1=sc.nextLine();
+     System.out.println("Enter 2nd string");
+     s2=sc.nextLine();
+     System.out.println("Concatenated string is");
+     System.out.println(s1+" "+s2);
+
+        }
+    }
